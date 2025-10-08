a2c72254c9bfcee2f36515aff11230fb35892cbd
Mridul Sharma <Mridul658@gmail.com>
2025-10-08 18:54:03 +0530

    LOGO UPDATED

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={lOGO} alt='Inscire' className='w-[124px]'/>
+      <img src={lOGO} alt='Inscire' className='w-[250px]'/>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, i) => (
           <li 
